2a6e3d9f63e5b43f45b48da53c444fa7bc904595
Lucas Perea <lucas.lerp@gmail.com>
2018-08-23 17:38:48 -0300

    Add endpoint to root

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var app = express()
 
+app.get('/', function(req, res) {
+  res.send('Hello world!')
+})
+
 app.get('/ping', function (req, res) {
   res.send('pong')
 })
